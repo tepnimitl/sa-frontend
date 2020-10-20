662a81041753412cb20747881fddb4db597cf7a0
tepnimitl <13294585+tepnimitl@users.noreply.github.com>
2020-10-19 21:36:15 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ class App extends Component {
                     <Paper zDepth={1} className="content">
                         <h2>Sentiment Analyser</h2>
                         <TextField ref={ref => this.textField = ref} onKeyUp={this.onEnterPress.bind(this)}
-                                   hintText="Type your sentence."/>
+                                   hintText="Type your sentence. ex: I love you."/>
                         <RaisedButton  label="Send" style={style} onClick={this.analyzeSentence.bind(this)}/>
                         {polarityComponent}
                     </Paper>
